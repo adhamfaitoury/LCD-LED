20d0be2d0aae676aafc71250b87bda8c4ada69fa
adhamfaitoury <adhamfaitoury>
2020-04-20 16:09:58 +0200

    Assignment

---
+++ main.c
@@ -0,0 +1,55 @@
+/*
+ * File:   main.c
+ * Author: adhamfaitoury
+ *
+ * Created on April 16, 2020, 12:01 AM
+ */
+#define F_CPU 16000000UL
+
+
+#include <avr/io.h> 
+#include <util/delay.h>
+#include "Config.h"
+#include "DIO.h"
+
+#define BUTTON         0
+#define LED            3
+
+#define portA           1
+#define portB           2
+#define portC           3
+#define portD           4
+
+int i = 0;
+
+int main(void) {
+
+    int data = 0b00000001;
+    PORTCas(OUT);
+    PORTDas(OUT);
+
+
+    while (1) {
+
+        for (i = 0; i < 7; i++) {
+            
+            setPORTD(data);
+            setPORTC(~data);
+            data = (data << 1);
+            _delay_ms(1000); // 1 second delay
+
+        }
+        data = 0x80;
+        for (i = 0; i < 7; i++) {
+            
+            setPORTD(data);
+            setPORTC(~data);
+            data = (data >> 1);
+            _delay_ms(1000); // 1 second delay
+
+        }
+
+        data = 1;
+    }
+}
+
